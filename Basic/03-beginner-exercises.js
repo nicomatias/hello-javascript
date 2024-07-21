dbd6814b4265fe67ec5de34eb9b07bb82bdfbd44
nllanosb <nllanosb@gmail.com>
2024-07-21 11:15:45 -0400

    .

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,14 +5,44 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Escribe un comentario en una línea
+// aqui escribo mi comentario
 
 // 2. Escribe un comentario en varias líneas
+/* aca empieza un comentario de varias lineas
+aca termina */
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+var numero_entero = 1;
+var booleano = true;
+var numero_decimal = 1.1
+var enteroGigante = 9999999999999999999999999999999999999999999999999999999999999
+var objeto = null
+var simbolo = Symbol("177072699")
+var texto = "texto"
+var undefinedValue
+
 // 4. Imprime por consola el valor de todas las variables
 
+console.log(numero_entero)
+console.log(booleano)
+console.log(numero_decimal)
+console.log(enteroGigante)
+console.log(objeto)
+console.log(simbolo)
+console.log(texto)
+console.log(undefinedValue)
+
 // 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof numero_entero)
+console.log(typeof booleano)
+console.log(typeof numero_decimal)
+console.log(typeof enteroGigante)
+console.log(typeof objeto)
+console.log(typeof simbolo)
+console.log(typeof texto)
+console.log(typeof undefinedValue)
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
